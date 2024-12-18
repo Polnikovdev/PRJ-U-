e4ccf179e5feaa63d053e41c4f3330cbb514cddd
Your Name <you@example.com>
2024-12-18 12:57:28 +0300

    pz9_part1

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from catalog import views
+#from . import views
 
 urlpatterns = [
     path("", views.index, name = "home"),
     path('admin/', admin.site.urls),
+    path("", views.index, name="index"),
 ]
